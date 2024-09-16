56c8a1eef91ccb09dc0f7b96145fdc7abfc4e65b
unknown <khoingo2003@gmail.com>
2024-09-16 15:45:29 +0700

    first commit

---
+++ KoiPondConstruct.Data/Repository/Impl/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using KoiPondConstruct.Data.Base;
+using KoiPondConstruct.Data.Models;
+
+namespace KoiPondConstruct.Data.Repository.Impl
+{
+    public class UserRepository : BaseRepository<TblUser>
+    {
+        public UserRepository() { }
+
+        public UserRepository(FA24_SE1702_PRN221_G2_KoiPondConstructContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ KoiPondConstruct.Data/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using KoiPondConstruct.Data.Models;
+using KoiPondConstruct.Data.Repository.Impl;
+
+
+namespace KoiPondConstruct.Data
+{
+    public class UnitOfWork
+    {
+        private FA24_SE1702_PRN221_G2_KoiPondConstructContext _context;
+        private UserRepository _userRepository;
+
+        public UnitOfWork()
+        {
+            _context ??= new FA24_SE1702_PRN221_G2_KoiPondConstructContext();// ??= meaning created new instance only if null
+
+        }
+
+        public UserRepository UserRepository
+        {
+            get
+            {
+                return _userRepository ??= new UserRepository(_context);
+
+            }
+        }
+    }
+}
